7b3d9f24d25d359e8034c5ec789adbe859d0ef0d
ElseVladimir <voinvova95@gmail.com>
2020-12-24 23:51:45 +0200

    move register_blueprint to main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from app import app
+from app import app,db #импортируем экземпляр app,db
+
+from posts.blueprint import posts
+#импорт экземпляра класса нашего блупринта
+
 import view
 
+app.register_blueprint(posts, url_prefix='/blog')
 
 if __name__ == "__main__":
 	app.run()
