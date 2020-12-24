ded3786058d948c7c387ca176c8db55b6803261d
ElseVladimir <voinvova95@gmail.com>
2020-12-24 23:54:30 +0200

    add SQLAlchemy configuration

--- app/config.py
+++ app/config.py
@@ -1,2 +1,6 @@
 class Configuration(object):
-	DEBUG = True
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # выпиливает deprecated warning
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1@localhost/test1'
+    # указываем какую бд хотим использовать,
+    # root:пароль, @localhost адрес сервера/имя базы данных
