3f9700d030842fea27b00e89655110b791475434
ElseVladimir <voinvova95@gmail.com>
2020-12-24 23:48:53 +0200

    move register_blueprint to main.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from config import Configuration
+from flask_sqlalchemy import SQLAlchemy # импортируем orm sqlalchemy
 
-from posts.blueprint import posts
-#импорт экземпляра класса нашего блупринта
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
-app.register_blueprint(posts, url_prefix='/blog')
+db = SQLAlchemy(app)
+
+
 #регистрируем блупринт, вторым аргументом указываем куда регистрируем( по какому урлу)
