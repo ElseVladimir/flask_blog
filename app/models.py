2d5a661980d18739edefec8a1d729e2f32822e45
ElseVladimir <voinvova95@gmail.com>
2020-12-26 02:07:50 +0200

    all commit info in changelog from Documentation.txt

--- app/models.py
+++ app/models.py
@@ -2,10 +2,16 @@ from app import db
 from datetime import datetime
 import re
 
+#заменяем все пробелы в заголовке на приемлемые для url-a,дабы потом использовать это в slug
 def slugify(s):
     pattern = r'[^\w+]'
     return re.sub(pattern, '-',s)
-    #заменяем все пробелы в заголовке на приемлемые для url-a,дабы потом использовать это в slug
+
+#создаст таблицу связи много-к-многим
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                     )
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +19,12 @@ class Post(db.Model):
     slug = db.Column(db.String(140), unique=True)
     body = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.now())
+
+    #устанавливаем отношения между двумя моделями, 1й аргумент название класса с которым выстраиваем отношение,
+    #вторым аргументом таблица содержащая данные много-к-многим. backref обратная ссылка, тоесть все посты приобретают
+    #свойства tags (post1.tags при его чтении получим список ассоциированных с этим постом тэгов).Аргумент
+    #lazy dynamic позволяет получить обьет BaseQuery c дополнительными свойствами и методами
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
 
     def __init__(self, *args, **kwargs):
         super(Post,self).__init__(*args, **kwargs)
@@ -23,9 +35,22 @@ class Post(db.Model):
             self.slug = slugify(self.title)
             #генерируем человекочитаемые uri
 
+    #__repr__ отвечает за представление из консоли,
+    # сама же функция выводит информацию в читаемом виде
     def __repr__(self):
         return '<Post id: {}, title: {}>'.format(self.id, self.title)
-        #__repr__ отвечает за представление,
-        # сама же функция выводит информацию в читаемом вдие
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+    def __init__(self, *args, **kwargs):
+        super(Tag,self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag id: {}, name: {}>'.format(self.id, self.name)
 
 #db.create_all()
--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 #тут хранятся изолированный кусок кода(blueprint), не забыть зарегестрировать блуприн в app.py
 from flask import render_template
-from models import Post
+from models import Post, Tag
 
 
 posts = Blueprint('posts', __name__, template_folder='templates')
@@ -18,4 +18,12 @@ def index():
 def post_detail(slug):
     post = Post.query.filter(Post.slug==slug).first()
     #переменная post уходит в шаблон
-    return render_template('posts/post_detail.html',post=post)
+    tags = post.tags #получаем закрепленные за постами тэги
+    return render_template('posts/post_detail.html',post=post, tags=tags)
+
+#http://localhost:5000/blog/tag/python
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug==slug).first()
+    posts = tag.posts.all() #получаем список а не обьект basequery
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
